c3b3d343cea11e54c97b70ad659e0285d05b44b9
Piperkfb <piperkfb@gmail.com>
2023-09-07 15:07:33 -0500

    Q3

---
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _3_writeline
+{
+    class Write
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle")
+        }
+    }
+}
+
