813605c7539eeb95c69127a2c450ccb25db3a5e1
Piperkfb <piperkfb@gmail.com>
2023-09-07 17:53:01 -0500

    almsot

--- csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -1,18 +1,15 @@
-﻿using System;
-
-class Program
+﻿﻿using System;
+class Array
 {
-    static void PrintArray(int[] array)
+    public static int[] ReplaceElement(int[] array, int index, int n)
     {
-        int i;
 
-        for (i = 0; i < array.Length; i++)
+        if(index < 0 || index > array.Length - 1)
         {
-            Console.Write(array[i]);
-            if (i < array.Length - 1)
-                Console.Write(" ");
+            Console.WriteLine("Index out of range");
+            return array;
         }
-
-        Console.WriteLine();
+        array[index] = n;
+        return array;
     }
 }
