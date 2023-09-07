933f43b2512d30a4bc0b44ba4f4d554c682b73b4
Piperkfb <piperkfb@gmail.com>
2023-09-07 17:45:07 -0500

    changed

--- csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -2,15 +2,6 @@
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        int[] array = {0, 1, 2, 3, 4, 5, 6};
-
-        PrintArray(array);
-        Array.ReplaceElement(array, 1, 98);
-        PrintArray(array);
-    }
-
     static void PrintArray(int[] array)
     {
         int i;
