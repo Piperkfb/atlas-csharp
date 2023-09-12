8e51defb4d6acf4b2f8344b512bc8889b19cea9d
Piperkfb <piperkfb@gmail.com>
2023-09-12 15:20:01 -0500

    Q 1, firsxt att

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        res = 0;
+
+        try //attempt division
+        {
+            res = a / b;    
+        }
+        catch(Exception e)
+        {
+            //Print error
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            //Print equation
+            Console.WriteLine("{0} / {1} = {2}", a, b, res);
+        }
+    }
+}
