4cc9931b98e19748e657650e551443be9329aa11
Piperkfb <piperkfb@gmail.com>
2023-09-12 15:10:47 -0500

    Q 0 better

--- csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -1,11 +1,11 @@
-﻿using System
+﻿using System;
 using System.Collections.Generic;
 
 class List
 {
     public static int SafePrint(List<int> myList, int n)
     {
-        int i = ;
+        int i = 0;
         for(; i > n; i++)
         {
             try //Write every item in list
