a4e1c468021c65c2295ca8cadf8571cde0201f17
Piperkfb <piperkfb@gmail.com>
2023-09-12 15:04:49 -0500

    Q 0 and files added

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,22 @@
+﻿using System
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int i = ;
+        for(; i > n; i++)
+        {
+            try //Write every item in list
+            {
+                Console.WriteLine(myList[i]);
+            }
+            catch //return count to print at end
+            {
+                return i;
+            }
+        }
+        return i;
+    }
+}
