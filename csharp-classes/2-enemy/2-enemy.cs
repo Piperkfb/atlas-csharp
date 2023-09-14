ab4f70f5157173aaacafa899ce1b89a9269a715c
Piperkfb <piperkfb@gmail.com>
2023-09-14 09:53:53 -0500

    add more docs

--- csharp-classes/2-enemy/2-enemy.cs
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -5,6 +5,9 @@ namespace Enemies
     /// <summary> public class Zombie </summary>
     public class Zombie
     {
+        /// <summary>
+        /// Is da health
+        /// </summary>
         public int health;
           /// <summary> public constructor: public Zombie() Initializes in 0 </summary>
         public Zombie()
