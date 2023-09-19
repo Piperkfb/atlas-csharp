5110eff0415d77e40b0ed20de24cb87ee8b82906
Piperkfb <piperkfb@gmail.com>
2023-09-19 14:15:50 -0500

    Q2

--- csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -1,17 +1,42 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
-namespace MyMath.Tests;
-
-public class Tests
+namespace MyMath.Tests
 {
-    [SetUp]
-    public void Setup()
+    [TestFixture]
+    public class MaxTests
     {
-    }
+        [Test]
+        public void MultipleRange()
+        {
+            List<int> listy = new List<int> {0, -24, 5, -3, 6};
+            int res = Operations.Max(listy);
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+            Assert.AreEqual(6, res);
+        }
+        [Test]
+        public void Negatives()
+        {
+            List<int> listy = new List<int> {-15, -3, -7, -30};
+            int res = Operations.Max(listy);
+
+            Assert.AreEqual(-3, res);
+        }
+        [Test]
+        public void Positives()
+        {   
+            List<int> listy = new List<int> {4, 10, 0, 34, 2};
+            int res = Operations.Max(listy);
+
+            Assert.AreEqual(34, res);
+        }
+        [Test]
+        public void EmptyList()
+        {            
+            List<int> listy = null;
+            int res = Operations.Max(listy);
+
+            Assert.AreEqual(0, res); 
+        }
     }
 }
--- csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -1,5 +1,20 @@
-﻿namespace MyMath;
-public class Class1
-{
+﻿using System;
 
+namespace MyMath
+{
+    public class Operations
+    {
+        public static int Max(List<int> nums)
+        {
+            if (nums == null || nums.Count <= 0)
+                return(0);
+            int maxi = nums[0];
+            foreach(int i in nums)
+            {
+                if (i > maxi)
+                    maxi = i;
+            }
+            return(maxi);
+        }
+    }
 }
