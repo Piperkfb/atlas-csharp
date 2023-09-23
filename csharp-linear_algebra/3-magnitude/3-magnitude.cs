d72d8856e77eee7cf427924c2b326f8f6da78a51
Piperkfb <piperkfb@gmail.com>
2023-09-23 13:55:15 -0500

    there we go! q3

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -7,8 +7,8 @@ public class VectorMath
         if(vector.Length == 3)
         {
             double bsq = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2);
-            double res = Math.Sqrt(bsq);
-            return bsq;
+            double res = Math.Round(Math.Sqrt(bsq), 2);
+            return res;
         //|v| = √(x^2 + y^2 + z^2)
         //In this case, v = (7, -3, -9), so:
         //|v| = √(7^2 + (-3)^2 + (-9)^2)
@@ -16,8 +16,8 @@ public class VectorMath
         else if(vector.Length == 2)
         {
             double bsq = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2);
-            double res = Math.Sqrt(bsq);
-            return bsq;
+            double res = Math.Round(Math.Sqrt(bsq), 2);
+            return res;
         //|v| = √(x^2 + y^2)
         //In this case, v = (3, 9), so:
         //|v| = √(3^2 + 9^2)
