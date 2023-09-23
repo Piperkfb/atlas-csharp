ae23187bd3fb2fde419e34d8457e6c7767248c93
Piperkfb <piperkfb@gmail.com>
2023-09-23 14:10:44 -0500

    Q6

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,9 +1,19 @@
 ﻿using System;
 
+/// <summary>
+/// class for meth of vectors
+/// </summary>
 public class VectorMath
 {
+    /// <summary>
+    /// Add two vectors
+    /// </summary>
+    /// <param name="vector1"></param>
+    /// <param name="vector2"></param>
+    /// <returns></returns>
     public static double[] Add(double[] vector1, double[] vector2)
     {
         double[] res = {vector1[0] + vector2[0], vector1[1] + vector2[1]};
+        return res;
     }
 }
