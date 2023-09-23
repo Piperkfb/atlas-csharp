a4688647166777cfa97e0fa141a960574f30aa67
Piperkfb <piperkfb@gmail.com>
2023-09-23 13:54:03 -0500

    hold on

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,22 +1,30 @@
 ﻿using System;
 
-public class VactorMath
+public class VectorMath
 {
     public static double Magnitude(double[] vector)
     {
-        if(vector.Length == 2)
+        if(vector.Length == 3)
         {
+            double bsq = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2);
+            double res = Math.Sqrt(bsq);
+            return bsq;
         //|v| = √(x^2 + y^2 + z^2)
         //In this case, v = (7, -3, -9), so:
         //|v| = √(7^2 + (-3)^2 + (-9)^2)
         }
-        else if(vector.Length == 3)
+        else if(vector.Length == 2)
         {
+            double bsq = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2);
+            double res = Math.Sqrt(bsq);
+            return bsq;
         //|v| = √(x^2 + y^2)
         //In this case, v = (3, 9), so:
         //|v| = √(3^2 + 9^2)
         }
         else
-        {}
+        {
+            return -1;
+        }
     }
 }
