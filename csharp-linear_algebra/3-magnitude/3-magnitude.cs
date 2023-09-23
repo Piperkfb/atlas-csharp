7c0c1f8491bf331ab63294089f12d9cf28b27747
Piperkfb <piperkfb@gmail.com>
2023-09-23 13:34:12 -0500

    added new lines and rounded decimal

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -4,12 +4,19 @@ public class VactorMath
 {
     public static double Magnitude(double[] vector)
     {
+        if(vector.Length == 2)
+        {
         //|v| = √(x^2 + y^2 + z^2)
         //In this case, v = (7, -3, -9), so:
         //|v| = √(7^2 + (-3)^2 + (-9)^2)
-
+        }
+        else if(vector.Length == 3)
+        {
         //|v| = √(x^2 + y^2)
         //In this case, v = (3, 9), so:
         //|v| = √(3^2 + 9^2)
+        }
+        else
+        {}
     }
 }
