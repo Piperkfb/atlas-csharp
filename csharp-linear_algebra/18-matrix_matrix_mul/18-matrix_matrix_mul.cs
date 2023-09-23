4e1272294f1a89afbd1124737910db60be08e016
Piperkfb <piperkfb@gmail.com>
2023-09-23 16:53:59 -0500

    q18

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -13,6 +13,40 @@ public class MatrixMath
     /// <returns></returns>
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
+        if(matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2 &&
+        matrix2.GetLength(0) == 2 && matrix2.GetLength(1) == 2)
+        {
+            double[,] res = {
+                {0, 0},
+                {0, 0}
+            };
+            res[0, 0] = (matrix1[0, 0] * matrix2[0, 0]) + (matrix1[0, 1] * matrix2[1, 0]) 
+            res[1, 0] = (matrix1[0, 0] * matrix2[0, 1]) + (matrix1[0, 1] * matrix2[1, 1])
+            res[0, 1] = (matrix1[1, 0] * matrix2[0, 0]) + (matrix1[1, 1] * matrix2[0, 1])
+            res[1, 1] = (matrix1[0, 1] * matrix2[0, 1]) + (matrix1[1, 1] * matrix2[1, 1])
 
+            return res;
+        }
+        else if(matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+        {
+            double[,] res = {
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+            };
+            res[0, 0] = (matrix1[0, 0] * matrix2[0, 0]) + (matrix1[1, 0] * matrix2[0, 1]) + (matrix1[2, 0] * matrix2[0, 2]); 
+            res[1, 0] = (matrix1[0, 0] * matrix2[0, 1]) + (matrix1[1, 0] * matrix2[1, 1]) + (matrix1[2, 0] * matrix2[1, 2]); 
+            res[2, 0] = (matrix1[0, 0] * matrix2[0, 2]) + (matrix1[1, 0] * matrix2[2, 1]) + (matrix1[2, 0] * matrix2[2, 2]); 
+            res[0, 1] = (matrix1[0, 1] * matrix2[0, 0]) + (matrix1[1, 1] * matrix2[0, 1]) + (matrix1[2, 1] * matrix2[0, 2]); 
+            res[1, 1] = (matrix1[0, 1] * matrix2[0, 1]) + (matrix1[1, 1] * matrix2[1, 1]) + (matrix1[2, 1] * matrix2[1, 2]); 
+            res[2, 1] = (matrix1[0, 1] * matrix2[0, 2]) + (matrix1[1, 1] * matrix2[2, 1]) + (matrix1[2, 1] * matrix2[2, 2]); 
+            res[0, 2] = (matrix1[0, 2] * matrix2[0, 0]) + (matrix1[1, 2] * matrix2[0, 1]) + (matrix1[2, 2] * matrix2[0, 2]); 
+            res[1, 2] = (matrix1[0, 2] * matrix2[0, 1]) + (matrix1[1, 2] * matrix2[1, 1]) + (matrix1[2, 2] * matrix2[1, 2]); 
+            res[2, 2] = (matrix1[0, 2] * matrix2[0, 2]) + (matrix1[1, 2] * matrix2[2, 1]) + (matrix1[2, 2] * matrix2[2, 2]); 
+            
+            return res;
+        }
+        else
+            return new double[,] {{-1}};
     }
 }
