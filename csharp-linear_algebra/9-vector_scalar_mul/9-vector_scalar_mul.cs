d5bd438dc71443a2bf80d79c866090d7270e9d88
Piperkfb <piperkfb@gmail.com>
2023-09-23 14:25:04 -0500

    q6 try

--- csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -4,6 +4,12 @@ public class VectorMath
 {
     public static double[] Multiply(double[] vector, double scalar)
     {
-        
+        //v = (98, 972)
+        //x = 0.5
+
+        //Multiply each component of v by x:
+
+        //v * x = (98 * 0.5, 972 * 0.5)
+        //v * x = (49, 486)
     }
 }
