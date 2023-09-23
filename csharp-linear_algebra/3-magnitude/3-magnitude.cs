09c7d92a60a56c12c7121103a55d93a4c6ea4577
Piperkfb <piperkfb@gmail.com>
2023-09-23 13:28:02 -0500

    added qs and 012

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class VactorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        //|v| = √(x^2 + y^2 + z^2)
+        //In this case, v = (7, -3, -9), so:
+        //|v| = √(7^2 + (-3)^2 + (-9)^2)
+
+        //|v| = √(x^2 + y^2)
+        //In this case, v = (3, 9), so:
+        //|v| = √(3^2 + 9^2)
+    }
+}
---
+++ csharp-linear_algebra/README.md
@@ -0,0 +1,1 @@
+Finally... My colledge level math skills at play :D. I sacrificed my spelling for dis!
