d8d5520c14844f0850d9144f9e9fd287792031b0
Piperkfb <piperkfb@gmail.com>
2023-09-24 12:41:36 -0500

    complete change of 18j

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -13,41 +13,22 @@ public class MatrixMath
     /// <returns></returns>
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
-        if(matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2 &&
-        matrix2.GetLength(0) == 2 && matrix2.GetLength(1) == 2)
-        {
-            double[,] res = {
-                {0, 0},
-                {0, 0}
-            };
-            res[0, 0] = (matrix1[0, 0] * matrix2[0, 0]) + (matrix1[1, 0] * matrix2[0, 1]);
-            res[1, 0] = (matrix1[0, 0] * matrix2[1, 0]) + (matrix1[1, 0] * matrix2[1, 1]);
-            res[0, 1] = (matrix1[0, 1] * matrix2[0, 0]) + (matrix1[1, 1] * matrix2[0, 1]);
-            res[1, 1] = (matrix1[0, 1] * matrix2[1, 0]) + (matrix1[1, 1] * matrix2[1, 1]);
+		int row1 = matrix1.GetLength(0);
+		int col1 = matrix1.GetLength(1);
+		int row2 = matrix2.GetLength(0);
+		int col2 = matrix2.GetLength(1);
+        double[,] matrix3 = new double[row1, col2];
+        double[,] MT = {{-1}};
 
-            return res;
-        }
-        else if(matrix1.GetLength(0) == 3 && matrix1.GetLength(1) == 3 &&
-        matrix2.GetLength(0) == 3 && matrix2.GetLength(1) == 3)
-        {
-            double[,] res = {
-                {0, 0, 0},
-                {0, 0, 0},
-                {0, 0, 0}
-            };
-            res[0, 0] = (matrix1[0, 0] * matrix2[0, 0]) + (matrix1[1, 0] * matrix2[0, 1]) + (matrix1[2, 0] * matrix2[0, 2]); 
-            res[1, 0] = (matrix1[0, 0] * matrix2[0, 1]) + (matrix1[1, 0] * matrix2[1, 1]) + (matrix1[2, 0] * matrix2[1, 2]); 
-            res[2, 0] = (matrix1[0, 0] * matrix2[0, 2]) + (matrix1[1, 0] * matrix2[2, 1]) + (matrix1[2, 0] * matrix2[2, 2]); 
-            res[0, 1] = (matrix1[0, 1] * matrix2[0, 0]) + (matrix1[1, 1] * matrix2[0, 1]) + (matrix1[2, 1] * matrix2[0, 2]); 
-            res[1, 1] = (matrix1[0, 1] * matrix2[0, 1]) + (matrix1[1, 1] * matrix2[1, 1]) + (matrix1[2, 1] * matrix2[1, 2]); 
-            res[2, 1] = (matrix1[0, 1] * matrix2[0, 2]) + (matrix1[1, 1] * matrix2[2, 1]) + (matrix1[2, 1] * matrix2[2, 2]); 
-            res[0, 2] = (matrix1[0, 2] * matrix2[0, 0]) + (matrix1[1, 2] * matrix2[0, 1]) + (matrix1[2, 2] * matrix2[0, 2]); 
-            res[1, 2] = (matrix1[0, 2] * matrix2[0, 1]) + (matrix1[1, 2] * matrix2[1, 1]) + (matrix1[2, 2] * matrix2[1, 2]); 
-            res[2, 2] = (matrix1[0, 2] * matrix2[0, 2]) + (matrix1[1, 2] * matrix2[2, 1]) + (matrix1[2, 2] * matrix2[2, 2]); 
-            
-            return res;
-        }
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
+            return MT;
         else
-            return new double[,] {{-1}};
+		{
+			for (int i = 0; i < row1; i++)
+				for (int j = 0; j < col2; j++)
+					for (int k = 0; k < col1; k++)
+						matrix3[i, j] += matrix1[i, k] * matrix2[k, j];
+			return matrix3;
+        }
     }
 }
