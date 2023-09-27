03aebfddff7bbacd5233b20253a3819a1949423d
Piperkfb <piperkfb@gmail.com>
2023-09-27 12:20:09 -0500

    q25

---
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <summary>
+/// Math for Matrix
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Transposes a mtrix
+    /// </summary>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        double[,] result = new double[cols, rows];
+
+        for (int x = 0; x < rows; x++)
+            for (int y = 0; y < cols; y++)
+                restult[y, x] = matrix[x, y];
+        return result;
+    }
+}
