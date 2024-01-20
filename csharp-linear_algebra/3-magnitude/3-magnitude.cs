3140cee0ea6a077de0d7a18944feb6aee4f61074
piperkfb <piperkfb@gmail.com>
2024-01-20 16:16:07 -0600

    Text answers

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _3_magnitude
+{
+    class VectorMath
+    {
+        public static double Magnitude(double[] vector)
+        {
+            //determine if 2d or 3d
+            //if neither, return -1
+            //2d equation (.00)
+            //3d equation (.00)
+        }
+    }
+}
---
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _6_vector_addition
+{
+    class VectorMath
+    {
+        public static double[] Add(double[] vector1, double[] vector2)
+        {
+            // 2d or 3d
+            // uneven sizes return -1
+            //add vectors
+        }
+    }
+}
