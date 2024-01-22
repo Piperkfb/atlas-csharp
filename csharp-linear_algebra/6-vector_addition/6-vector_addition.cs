536a499966042bcbdab03bbf352f4e8f68f6c272
piperkfb <piperkfb@gmail.com>
2024-01-22 15:02:53 -0600

    Q56

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 
-namespace _6_vector_addition
+/// <summary>
+/// Class Vector math
+/// </summary>
+class VectorMath
 {
-    class VectorMath
+    /// <summary>
+    /// Add two vectors
+    /// </summary>
+    public static double[] Add(double[] vector1, double[] vector2)
     {
-        public static double[] Add(double[] vector1, double[] vector2)
+        // 2d or 3d
+        if (vector1.Length == 2 && vector2.Length == 2)
         {
-            // 2d or 3d
-            // uneven sizes return -1
             //add vectors
+            double[] results = (0, 0);
+            for (i = 0; i <= 1; i++)
+                {
+                    results[i] = Math.Add(vector1[i], vector2[i]);
+                    return results;
+                }
         }
-    }
+
+        else if (vector1.Length == 3 && vector2.Length == 3)
+        {
+            //add vectors
+            double[] results = (0, 0, 0);
+            for (i = 0; i <= 2; i++)
+                {
+                    results[i] = Math.Add(vector1[i], vector2[i]);
+                    return results;
+                }
+        }
+        // uneven sizes return -1
+        return new double[] (-1);
+    } 
 }
