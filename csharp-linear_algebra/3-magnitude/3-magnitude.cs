f267f1521c26953516eeb3aa5db59c4e5d19ed1c
piperkfb <piperkfb@gmail.com>
2024-01-22 14:33:27 -0600

    NOW to hundreth

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -13,13 +13,13 @@ class VectorMath
         if (vector.Length == 2)
         {
             double cSquared = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2);
-            return Math.Round(Math.Sqrt(cSquared));
+            return Math.Round(Math.Sqrt(cSquared), 2);
         }
         //determine if 2d or 3d
         else if (vector.Length == 3)
         {
             double cSquared = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2);
-            return Math.Round(Math.Sqrt(cSquared));
+            return Math.Round(Math.Sqrt(cSquared), 2);
         }
         //if neither, return -1
         else
