1396f1a30156f1b9730cf47f009460bb3f39cf4d
piperkfb <piperkfb@gmail.com>
2024-01-22 14:24:37 -0600

    no namespace?

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,27 +1,24 @@
 ﻿using System;
 
-namespace _3_magnitude
-{
-    class VectorMath
+class VectorMath
     {
-        public static double Magnitude(double[] vector)
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length == 2)
         {
-            if (vector.Length == 2)
-            {
-                double cSquared = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2);
-                return Math.Sqrt(cSquared);
-            }
-            //determine if 2d or 3d
-            else if (vector.Length == 3)
-            {
-                double cSquared = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2);
-                return Math.Sqrt(cSquared);
-            }
-            //if neither, return -1
-            else
-            {
-                return (-1);
-            }
+            double cSquared = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2);
+            return Math.Sqrt(cSquared);
+        }
+        //determine if 2d or 3d
+        else if (vector.Length == 3)
+        {
+            double cSquared = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2);
+            return Math.Sqrt(cSquared);
+        }
+        //if neither, return -1
+        else
+        {
+            return (-1);
         }
     }
 }
