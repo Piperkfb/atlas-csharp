992638a4a027c4ffa15ed280a911516dd411fad9
piperkfb <piperkfb@gmail.com>
2024-01-22 17:49:24 -0600

    [A[Bwrong return typr

--- csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -12,12 +12,12 @@ class VectorMath
     {
         if (vector1.Length == 2 && vector2.Length == 2)
         {
-            int results = (vector1[0] * vector2[0]) + (vector1[1] * vector2[1]);
+            double results = (vector1[0] * vector2[0]) + (vector1[1] * vector2[1]);
             return results;
         }
         else if (vector1.Length == 3 && vector2.Length == 3)
         {
-            int result = (vector1[0] * vector2[0]) + (vector1[1] * vector2[1]) + (vector1[2] * vector2[2]);
+            double result = (vector1[0] * vector2[0]) + (vector1[1] * vector2[1]) + (vector1[2] * vector2[2]);
             return results;
         }
         return (-1);
