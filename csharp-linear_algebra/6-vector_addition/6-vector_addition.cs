28d28b9296ec853cb162ca9f8dbef362e5e52c80
piperkfb <piperkfb@gmail.com>
2024-01-22 15:12:03 -0600

    int i

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -15,7 +15,7 @@ class VectorMath
         {
             //add vectors
             double[] results = (0, 0);
-            for (i = 0; i <= 1; i++)
+            for (int i = 0; i <= 1; i++)
                 {
                     results[i] = Math.Add(vector1[i], vector2[i]);
                     return results;
@@ -26,7 +26,7 @@ class VectorMath
         {
             //add vectors
             double[] results = (0, 0, 0);
-            for (i = 0; i <= 2; i++)
+            for (int i = 0; i <= 2; i++)
                 {
                     results[i] = Math.Add(vector1[i], vector2[i]);
                     return results;
