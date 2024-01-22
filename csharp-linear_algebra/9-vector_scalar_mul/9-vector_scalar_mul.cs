a70b246554d6781214078f091268eb4559969951
piperkfb <piperkfb@gmail.com>
2024-01-22 17:27:17 -0600

    Q9

--- csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -11,12 +11,25 @@ class VectorMath
     public static double[] Multiply(double[] vector, double scalar)
     {
         // 2d or 3d
-        double[] results = {0, 0}
-        for (int i = 0; i <= 2; i++)
+        if (vector.Length == 2)
         {
-            results[i] = (vector[i] * scalar);
+            double[] results = {0, 0};
+            for (int i = 0; i <= 1; i++)
+            {
+                results[i] = (vector[i] * scalar);
+            }
             return results;
         }
+        else if (vector.Length == 3)
+        {
+            double[] results = {0, 0, 0};
+            for (int i = 0; i <= 2; i++)
+            {
+                results[i] = (vector[i] * scalar);
+            }
+            return results;
+        } 
         return new double[] {-1};
     }
+       
 }
