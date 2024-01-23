e1483c09acce637cc824387ad651faa3cc4cbd90
piperkfb <piperkfb@gmail.com>
2024-01-23 17:17:29 -0600

    q1

--- csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
+++ csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -1,12 +1,59 @@
 ﻿using System;
 
-namespace _1_damage_delegation
+/// <summary>
+/// class player
+/// </summary>
+public class Player
 {
-    class Program
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary>A player's class</summary>
+
+    public Player(string name = "Player", float maxHp = 100f)
     {
-        static void Main(string[] args)
+        if (maxHp <= 0)
         {
-            Console.WriteLine("Hello World!");
+            maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
         }
+        
+        this.maxHp = maxHp;
+        this.name = name;
+        this.hp = maxHp;
+        
+    }
+
+    delegate void CalculateHealth(float health);
+
+    public void TakeDamage(float damage)
+    {
+        if (damage <= 0)
+        {
+            Console.WriteLine("{0} takes 0 damage!", this.name);
+        }
+        else
+        {
+            Console.WriteLine("{0} takes {1} damage!", this.name, damage);
+            this.hp -= damage;
+        }
+    }
+    public void HealDamage(float heal)
+    {
+        if (heal <= 0)
+        {
+            Console.WriteLine("{0} heals 0 HP!", this.name);
+        }
+        else
+        {
+            Console.WriteLine("{0} heals {1} HP!", this.name, heal);
+            this.hp += heal;
+        }
+    }
+    /// <summary>prints health</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
     }
 }
