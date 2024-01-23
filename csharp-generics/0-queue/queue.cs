0a65dba245da87ab8d2a46ae4693fa8be5247a7b
piperkfb <piperkfb@gmail.com>
2024-01-23 14:15:06 -0600

    maybe?

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 
+/// <summary>
+/// class Queue<t>
+/// </summary>
 class Queue<T>
 {
+    /// <summary>
+    /// Check type of T
+    /// </summary>
     public Type CheckType()
     {
         return typeof(T);
--- csharp-generics/1-enqueue/queue.cs
+++ csharp-generics/1-enqueue/queue.cs
@@ -1,13 +1,43 @@
 ﻿using System;
 
+/// <summary>
+/// class Queue<t>
+/// </summary>
 class Queue<T>
 {
+    public Node head;
+    public Node tail;
+    public int count;
+
+    /// <summary> creation of node </summary>
     public class Node
     {
+        private T value = null;
+        public Node next = null;
+
+        public Node(T thing)
+            value = thing;
 
     }
-    public Type CheckType()
+
+    public void Enqueue()
     {
-        return typeof(T);
+        Node newNode = new Node(value);
+        if (head == null)
+            head = newNode;
+
+        else
+            tail.next = newNode;
+        
+        count++;
+
     }
+
+    /// <summary> Check type of T </summary>
+    public Type CheckType()
+        return typeof(T);
+
+    /// <summary> returns count </summary>
+    public int Count()
+        return count;
 }
