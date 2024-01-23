f4b948d30d00da7bf21a2619ef64707f99083ad2
piperkfb <piperkfb@gmail.com>
2024-01-23 14:58:00 -0600

    woot woot?

---
+++ csharp-generics/4-print/queue.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+/// <summary>
+/// class Queue t
+/// </summary>
+class Queue<T>
+{
+    public Node head;
+    public Node tail;
+    public int count;
+
+    /// <summary> creation of node </summary>
+    public class Node
+    {
+        public T value;
+        public Node next = null;
+
+        public Node(T stri)
+        {
+            value = stri;
+        }
+
+    }
+
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+            
+        }
+        
+        count++;
+    }
+
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        else
+        {
+            tail.value = head.value;
+            head = head.next;
+            count--;
+            return tail.value;
+        }
+    }
+
+    public T Peek()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        else
+        {
+            return head.value;
+        }
+    }
+
+    public void Print()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            tail = head;
+            while (tail != null)
+            {
+                Console.WriteLine(tail.value);
+                tail = tail.next;
+            }
+        }
+    }
+
+    /// <summary> Check type of T </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// <summary> returns count </summary>
+    public int Count()
+    {
+        return count;
+    }
+}
