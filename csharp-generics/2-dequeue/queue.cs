98dee9b540d85c91ad7d223cdf3b1aa3130f1caa
piperkfb <piperkfb@gmail.com>
2024-01-23 14:30:23 -0600

    dequeue, easy?

---
+++ csharp-generics/2-dequeue/queue.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+/// <summary>
+/// class Queue t
+/// </summary>
+class Queue<T>
+{
+    public Node head;
+    public Node tail;
+    public int count;
+
+    /// <summary> creation of node </summary>
+    public class Node
+    {
+        private T value;
+        public Node next = null;
+
+        public Node(T stri)
+        {
+            value = stri;
+        }
+
+    }
+
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+
+        else
+            tail = newNode;
+            tail.next = newNode;
+        
+        count++;
+    }
+
+    public T Dequeue(T value)
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty")
+            return default(T);
+        }
+        else
+        {
+            tail.value = head.value;
+            head = head.next;
+            count==:
+            return tail.value;
+        }
+    }
+
+    /// <summary> Check type of T </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// <summary> returns count </summary>
+    public int Count()
+    {
+        return count;
+    }
+}
