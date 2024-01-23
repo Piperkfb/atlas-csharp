ab7bb862512b212d41726d4ed4efdc06cbf29dee
piperkfb <piperkfb@gmail.com>
2024-01-22 18:14:32 -0600

    Q14

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 
-namespace _14_matrix_addition
+/// <summary>
+/// class Matrix math
+/// </summary>
+class MatrixMath
 {
-    class Program
+    /// <summary>
+    /// 
+    /// </summary>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        static void Main(string[] args)
+        //2d or 3d
+        if (matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2 && 
+        matrix2.GetLength(0) == 2 && matrix2.GetLength(1) == 2)
         {
-            Console.WriteLine("Hello World!");
+            double[,] res = {{0, 0}, {0, 0}};
+            for (int i = 0; i <= 1; i++)
+                for (int y = 0; y <= 1; y++)
+                {
+                    res[i, y] = (matrix1[i, y] + matrix2[i, y]);
+                }
+            return res;
         }
+        else if (matrix1.GetLength(0) == 3 && matrix1.GetLength(1) == 3 && matrix1.GetLength(2) == 3 &&
+        matrix2.GetLength(0) == 3 && matrix2.GetLength(1) == 3 && matrix2.GetLength(2) == 3)
+        {
+            double[,] res = {{0, 0}, {0, 0}, {0, 0}};
+            for (int i = 0; i <= 2; i++)
+                for (int y = 0; y <= 2; y++)
+                {
+                    res[i, y] = (matrix1[i, y] + matrix2[i, y]);
+                }
+            return res;
+        }
+        return new double[,] {{-1}};
     }
 }
