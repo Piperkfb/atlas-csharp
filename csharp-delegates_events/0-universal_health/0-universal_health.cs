1390176d14cd3b9dd506c023c0b8573e29bfa9d2
piperkfb <piperkfb@gmail.com>
2024-01-23 15:53:06 -0600

    it work?

--- csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -6,25 +6,21 @@ public class Player
     private float maxHp;
     private float hp;
 
-    public Player(string name, float maxHp)
+    public Player(string name = "Player", float maxHp = 100f)
     {
-        this.name = "Player";
-        this.maxHp = (100f);
-
         if (maxHp <= 0)
         {
             this.maxHp = 100f;
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
         }
         
-        if (hp != maxHp)
-        {
-            this.hp = maxHp;
-        }
+        this.maxHp = maxHp;
+        this.name = name;
+        this.hp = maxHp;
         
     }
     public void PrintHealth()
     {
-        console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
+        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
     }
 }
