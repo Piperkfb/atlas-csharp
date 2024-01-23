c0918ac87ba085eca3dc4c91e07ea86a388b0ae0
piperkfb <piperkfb@gmail.com>
2024-01-22 20:16:28 -0600

    q24

---
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MatrixMath
+{
+       public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        double[,] res = new double[2,2];
+        double[,] shearMatrix;
+        double[,] fail;
+
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+        {
+            if (direction == 'x')
+                shearMatrix = new double[,] {{1, 0}, {factor, 1}};
+            else if (direction == 'y')
+                shearMatrix = new double[,] {{1, factor}, {0, 1}};
+        
+            for (int i = 0; i < 2; i++)
+			    for (int j = 0; j < 2; j++)
+				    for (int k = 0; k < 2; k++)
+					    res[i, j] += (matrix[i, k] * shearMatrix[k, j]);
+            return res;           
+        }
+		return fail;
+    }
+}
