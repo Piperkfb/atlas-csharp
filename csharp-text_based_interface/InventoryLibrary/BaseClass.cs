b37384346f6434785a770aefccfc2a2fa626310f
piperkfb <piperkfb@gmail.com>
2024-01-24 15:31:29 -0600

    working

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Base
+{
+	private String id;
+	private DateTime date_created;
+	private DateTime date_updated;
+}
---
+++ csharp-text_based_interface/InventoryLibrary/Item.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Item : Base
+{
+    public string name;
+	public string description;
+	public float price = 0.2f;
+	public List<String> tags;
+}
